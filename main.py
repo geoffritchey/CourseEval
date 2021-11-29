3b11060d0eb5034a6216bf8d07e1c93f555588c9
Geoffrey Ritchey <geoffrey.ritchey@gmail.com>
2021-11-29 17:14:57 -0600

    Instructor

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import build
 
 now = datetime.datetime.now()
 course_name = re.compile(r'(\D+)(\d+)')
+top="&$top=10&$count=true"
 
 if __name__ == '__main__':
     mem_conn = sqlite3.connect(':memory:')
@@ -134,6 +135,280 @@ if __name__ == '__main__':
     );
 
     ''')
+    mem_conn.execute('''
+    
+create table FacultyDemographics (
+    ExternalId int
+    ,PersonIdentifier int
+    ,EffectiveOn datetime
+    ,FirstName TEXT
+    ,LastName TEXT
+    ,EmailAddress1 TEXT
+    ,EmailAddress1IsPreferred boolean
+    ,EmailAddress2 TEXT
+    ,EmailAddress2IsPreferred boolean
+    ,EmailAddress3 TEXT
+    ,EmailAddress3IsPreferred boolean
+    ,PreferredFirstName TEXT
+    ,MiddleName TEXT
+    ,Suffix TEXT
+    ,DateOfBirth datetime
+    ,Gender TEXT
+    ,Sex TEXT
+    ,Race1 TEXT
+    ,Race2 TEXT
+    ,Race3 TEXT
+    ,Ethnicity1 TEXT
+    ,Ethnicity2 TEXT
+    ,Ethnicity3 TEXT
+    ,PrivacyElected boolean
+    ,PrimaryDepartment TEXT
+    ,Qualifications TEXT
+    ,RetiredOn date
+    ,TeachingStatus TEXT
+    ,Title TEXT
+    ,Address1AddressType TEXT
+    ,Address1Line1 TEXT
+    ,Address1Line2 TEXT
+    ,Address1Line3 TEXT
+    ,Address1City TEXT
+    ,Address1State TEXT
+    ,Address1County TEXT
+    ,Address1Country TEXT
+    ,Address1PostalCode TEXT
+    ,Address2AddressType TEXT
+    ,Address2Line1 TEXT
+    ,Address2Line2 TEXT
+    ,Address2Line3 TEXT
+    ,Address2City TEXT
+    ,Address2State TEXT
+    ,Address2County TEXT
+    ,Address2Country TEXT
+    ,Address2PostalCode TEXT
+    ,Address3AddressType TEXT
+    ,Address3Line1 TEXT
+    ,Address3Line2 TEXT
+    ,Address3Line3 TEXT
+    ,Address3City TEXT
+    ,Address3State TEXT
+    ,Address3County TEXT
+    ,Address3Country TEXT
+    ,Address3PostalCode TEXT
+    ,Campus1 TEXT
+    ,Campus2 TEXT
+    ,Campus3 TEXT
+    ,Telephone1CountryCode int
+    ,Telephone1AreaCode int
+    ,Telephone1Number int
+    ,Telephone1Extension int
+    ,Telephone1TelephoneType TEXT
+    ,Telephone1MobileCarrier TEXT
+    ,Telephone2CountryCode int
+    ,Telephone2AreaCode int
+    ,Telephone2Number int
+    ,Telephone2Extension int
+    ,Telephone2TelephoneType TEXT
+    ,Telephone2MobileCarrier TEXT
+    ,Telephone3CountryCode int
+    ,Telephone3AreaCode int
+    ,Telephone3Number int
+    ,Telephone3Extension int
+    ,Telephone3TelephoneType TEXT
+    ,Telephone3MobileCarrier TEXT
+    ,Username1 TEXT
+    ,Username2 TEXT
+    ,Username3 TEXT
+    ,Certification1 TEXT
+    ,Certification2 TEXT
+    ,Certification3 TEXT
+    ,Discipline1 TEXT
+    ,Discipline2 TEXT
+    ,Discipline3 TEXT
+    ,Department1 TEXT
+    ,Department2 TEXT
+    ,Department3 TEXT
+    ,Position1 TEXT
+    ,Position2 TEXT
+    ,Position3 TEXT
+    ,ManagingDepartment1 TEXT
+    ,ManagingDepartment2 TEXT
+    ,ManagingDepartment3 TEXT
+    ,School1 TEXT
+    ,School2 TEXT
+    ,School3 TEXT
+);
+
+    ''')
+    mem_conn.execute('''
+    
+create table StudentDemographics (
+    ExternalId int
+    ,PersonIdentifier int
+    ,EffectiveOn datetime
+    ,FirstName TEXT
+    ,LastName TEXT
+    ,EmailAddress1 TEXT
+    ,EmailAddress1IsPreferred boolean
+    ,EmailAddress2 TEXT
+    ,EmailAddress2IsPreferred boolean
+    ,EmailAddress3 TEXT
+    ,EmailAddress3IsPreferred boolean
+    ,PreferredFirstName TEXT
+    ,MiddleName TEXT
+    ,Suffix TEXT
+    ,DateOfBirth datetime
+    ,Gender TEXT
+    ,Sex TEXT
+    ,Race1 TEXT
+    ,Race2 TEXT
+    ,Race3 TEXT
+    ,Ethnicity1 TEXT
+    ,Ethnicity2 TEXT
+    ,Ethnicity3 TEXT
+    ,CardId int
+    ,SisId int
+    ,StudentId int
+    ,HousingFacility TEXT
+    ,PrivacyElected boolean
+    ,ActCompositeScore int
+    ,ActEnglishScore int
+    ,ActMathScore int
+    ,ActReadingScore int
+    ,ActScienceScore int
+    ,ActWritingScore int
+    ,SatScore int
+    ,SatMathScore int
+    ,SatVerbalScore int
+    ,SatWritingScore int
+    ,GreScore int
+    ,GmatScore int
+    ,LsatScore int
+    ,McatScore int
+    ,IncomingGpa numeric
+    ,CumulativeGpa numeric
+    ,IsMatriculating boolean
+    ,Certificate1 TEXT
+    ,Certificate2 TEXT
+    ,Certificate3 TEXT
+    ,Major1 TEXT
+    ,Major2 TEXT
+    ,Major3 TEXT
+    ,Minor1 TEXT
+    ,Minor2 TEXT
+    ,Minor3 TEXT
+    ,Concentration1 TEXT
+    ,Concentration2 TEXT
+    ,Concentration3 TEXT
+    ,DegreeProgram1 TEXT
+    ,DegreeProgram2 TEXT
+    ,DegreeProgram3 TEXT
+    ,SchoolOfEnrollment1 TEXT
+    ,SchoolOfEnrollment2 TEXT
+    ,SchoolOfEnrollment3 TEXT
+    ,DegreeType1 TEXT
+    ,DegreeType2 TEXT
+    ,DegreeType3 TEXT
+    ,AcademicStanding TEXT
+    ,AnticipatedGraduationDate date
+    ,CareerLevel TEXT
+    ,ClassStanding TEXT
+    ,CumulativeCreditHoursAttempted int
+    ,CumulativeCreditHoursCompleted int
+    ,CumulativeCreditsEarned int
+    ,CurrentCreditLoad int
+    ,EnrollmentStatus TEXT
+    ,InstitutionEntryTerm TEXT
+    ,PreviousInstitution1Name TEXT
+    ,PreviousInstitution1StartDate date
+    ,PreviousInstitution1EndDate date
+    ,PreviousInstitution2Name TEXT
+    ,PreviousInstitution2StartDate date
+    ,PreviousInstitution2EndDate date
+    ,PreviousInstitution3Name TEXT
+    ,PreviousInstitution3StartDate date
+    ,PreviousInstitution3EndDate date
+    ,AthleticProgram1 TEXT
+    ,AthleticProgram2 TEXT
+    ,AthleticProgram3 TEXT
+    ,HonorsProgram1 TEXT
+    ,HonorsProgram2 TEXT
+    ,HonorsProgram3 TEXT
+    ,FamilyMember1EducationLevel TEXT
+    ,FamilyMember1Relationship TEXT
+    ,FamilyMember2EducationLevel TEXT
+    ,FamilyMember2Relationship TEXT
+    ,FamilyMember3EducationLevel TEXT
+    ,FamilyMember3Relationship TEXT
+    ,PreEntryProgram1 TEXT
+    ,PreEntryProgram2 TEXT
+    ,PreEntryProgram3 TEXT
+    ,LocalResidenceLocation1 TEXT
+    ,LocalResidenceLocation2 TEXT
+    ,LocalResidenceLocation3 TEXT
+    ,AspiredEducationLevel TEXT
+    ,ForeignNationalStatus TEXT
+    ,HighSchool TEXT
+    ,HomeResidenceLocation TEXT
+    ,Hometown TEXT
+    ,ImmigrationStatus TEXT
+    ,IsCommuter boolean
+    ,IsFirstGeneration boolean
+    ,IsPellGrantEligible boolean
+    ,MilitaryStatus TEXT
+    ,Address1AddressType TEXT
+    ,Address1Line1 TEXT
+    ,Address1Line2 TEXT
+    ,Address1Line3 TEXT
+    ,Address1City TEXT
+    ,Address1State TEXT
+    ,Address1County TEXT
+    ,Address1Country TEXT
+    ,Address1PostalCode TEXT
+    ,Address2AddressType TEXT
+    ,Address2Line1 TEXT
+    ,Address2Line2 TEXT
+    ,Address2Line3 TEXT
+    ,Address2City TEXT
+    ,Address2State TEXT
+    ,Address2County TEXT
+    ,Address2Country TEXT
+    ,Address2PostalCode TEXT
+    ,Address3AddressType TEXT
+    ,Address3Line1 TEXT
+    ,Address3Line2 TEXT
+    ,Address3Line3 TEXT
+    ,Address3City TEXT
+    ,Address3State TEXT
+    ,Address3County TEXT
+    ,Address3Country TEXT
+    ,Address3PostalCode TEXT
+    ,Campus1 TEXT
+    ,Campus2 TEXT
+    ,Campus3 TEXT
+    ,Telephone1CountryCode int
+    ,Telephone1AreaCode int
+    ,Telephone1Number int
+    ,Telephone1Extension int
+    ,Telephone1TelephoneType TEXT
+    ,Telephone1MobileCarrier TEXT
+    ,Telephone2CountryCode int
+    ,Telephone2AreaCode int
+    ,Telephone2Number int
+    ,Telephone2Extension int
+    ,Telephone2TelephoneType TEXT
+    ,Telephone2MobileCarrier TEXT
+    ,Telephone3CountryCode int
+    ,Telephone3AreaCode int
+    ,Telephone3Number int
+    ,Telephone3Extension int
+    ,Telephone3TelephoneType TEXT
+    ,Telephone3MobileCarrier TEXT
+    ,Username1 TEXT
+    ,Username2 TEXT
+    ,Username3 TEXT
+);
+
+    ''')
 
     root_uri = "https://sisclientweb-100542.campusnexus.cloud/"
 
@@ -181,14 +456,14 @@ if __name__ == '__main__':
             "")
         rows = cur.fetchall()
         with open('AcademicPrograms.csv', 'w') as f:
-            print(",".join(("ProgramIdentifier", "OrgUnitIdentifier", "Name", "ProgramType,Description", "group_concat(CourseIdentifiers")))
+            print(",".join(("ProgramIdentifier", "OrgUnitIdentifier", "Name", "ProgramType,Description", "CourseIdentifiers")), file=f)
             for row in rows:
-                program_Identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Program/" + str(row[0])))
-                OrgUnitIdentifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Org/" + str(row[1])))
+                program_Identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Program/" + str(row[0])))
+                OrgUnitIdentifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Org/" + str(row[1])))
                 Name = row[2]
                 Type = row[3]
                 Description = row[4]
-                CourseIdentifiers = list(map(lambda param_x:str(uuid.uuid3(uuid.NAMESPACE_URL, "Course/" + str(param_x))),  row[5].split(",")))
+                CourseIdentifiers = list(map(lambda param_x:str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Course/" + str(param_x))),  row[5].split(",")))
                 print(",".join((program_Identifier, OrgUnitIdentifier, Name, Type, Description, ",".join(CourseIdentifiers))), file=f)
 
         '''
@@ -231,8 +506,8 @@ if __name__ == '__main__':
         with open('Enrollments.csv', 'w') as f:
             print(",".join(("PersonIdentifier", "SectionIdentifier", "Status", "FirstName", "LastName", "Email")), file=f)
             for row in rows:
-                person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Student/" + str(row[0])))
-                section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Section/" + str(row[1])))
+                person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Student/" + str(row[0])))
+                section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Section/" + str(row[1])))
                 status = row[2]
                 first_name = row[3]
                 last_name = row[4]
@@ -244,7 +519,7 @@ if __name__ == '__main__':
             '''
             instructor_uri = "{0}ds/campusnexus/StudentCourses?$expand=ClassSection($select=Id)," \
                           "ClassSection($expand=Instructor($select=StaffId)," \
-                          "Instructor($expand=Staff($select=FirstName,LastName,EmailAddress)))" \
+                          "Instructor($expand=Staff($select=FirstName,LastName,EmailAddress,CreatedDateTime)))" \
                           "&$filter=EndDate gt {1} and StartDate le {2}" \
                               "&$select=StudentId&$apply=groupby((ClassSection/Instructor/StaffId))" \
                               "".format(root_uri, now.strftime("%Y-%m-%d"),
@@ -267,6 +542,15 @@ if __name__ == '__main__':
                                     "Primary")
                                  )
 
+                mem_conn.execute("insert into FacultyDemographics(PersonIdentifier, EffectiveOn, FirstName, LastName, EmailAddress1) values (?,?,?,?,?)", (
+                    child["ClassSection"]["Instructor"]["StaffId"],
+                    child["ClassSection"]["Instructor"]["Staff"]["CreatedDateTime"],
+                    child["ClassSection"]["Instructor"]["Staff"]["FirstName"],
+                    child["ClassSection"]["Instructor"]["Staff"]["LastName"],
+                    child["ClassSection"]["Instructor"]["Staff"]["EmailAddress"],
+                    )
+                                 )
+
             cur = mem_conn.cursor()
             cur.execute(
                 "select distinct PersonIdentifier, SectionIdentifier, FirstName, LastName, Email, Role "
@@ -276,12 +560,29 @@ if __name__ == '__main__':
             with open('Instructors.csv', 'w') as f:
                 print(",".join(("PersonIdentifier", "SectionIdentifier", "FirstName", "LastName", "Email", "Role")), file=f)
                 for row in rows:
-                    person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Account/" + str(row[0])))
-                    section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Section/" + str(row[1])))
+                    person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Account/" + str(row[0])))
+                    section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Section/" + str(row[1])))
                     first_name = row[2]
                     last_name = row[3]
                     email = row[4]
                     print(",".join((person_identifier, section_identifier, first_name, last_name, email)), file=f)
+
+
+            cur = mem_conn.cursor()
+            cur.execute(
+                "select distinct PersonIdentifier, EffectiveOn, FirstName, LastName, EmailAddress1 "
+                "from FacultyDemographics "
+                "")
+            rows = cur.fetchall()
+            with open('FacultyDemographics.csv', 'w') as f:
+                print(",".join(("PersonIdentifier", "EffectiveOn", "FirstName", "LastName", "EmailAddress1")), file=f)
+                for row in rows:
+                    person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Account/" + str(row[0])))
+                    effective_on = row[1]
+                    first_name = row[2]
+                    last_name = row[3]
+                    email = row[4]
+                    print(",".join((person_identifier, effective_on, first_name, last_name, email)), file=f)
 
             '''
                     Sections
@@ -323,9 +624,9 @@ if __name__ == '__main__':
             with open('Sections.csv', 'w') as f:
                 print(",".join(("SectionIdentifier", "TermIdentifier", "CourseIdentifier", "SectionNumber", "BeginDate", "EndDate", "DeliveryMode")), file=f)
                 for row in rows:
-                    section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Section/" + str(row[0])))
-                    term_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Term/" + str(row[1])))
-                    course_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Course/" + str(row[2])))
+                    section_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Section/" + str(row[0])))
+                    term_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Term/" + str(row[1])))
+                    course_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Course/" + str(row[2])))
                     number = row[3]
                     begin_date = row[4]
                     end_date = row[5]
@@ -374,11 +675,11 @@ if __name__ == '__main__':
             with open('Course.csv', 'w') as f:
                 print(",".join(("CourseIdentifier", "Subject", "CourseNumber", "Title", "OrgUnitIdentifier", "CourseType")), file=f)
                 for row in rows:
-                    course_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Course/" + str(row[0])))
+                    course_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Course/" + str(row[0])))
                     subject = row[1]
                     number = row[2]
                     title = row[3]
-                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Org/" + str(row[4])))
+                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Org/" + str(row[4])))
                     course_type = row[5]
                     print(",".join((course_identifier, subject, number, title, org_unit_identifier, course_type)), file=f)
 
@@ -419,7 +720,7 @@ if __name__ == '__main__':
             with open('AcademicTerm.csv', 'w') as f:
                 print(",".join(("TermIdentifier", "Name", "BeginDate", "EndDate", "Type")), file=f)
                 for row in rows:
-                    term_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Term/" + str(row[0])))
+                    term_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Term/" + str(row[0])))
                     name = row[1]
                     begin_date = row[2]
                     end_date = row[3]
@@ -461,7 +762,7 @@ if __name__ == '__main__':
             rows = cur.fetchall()
             with open('OrganizationalUnits.csv', 'w') as f:
                 for row in rows:
-                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Org/" + str(row[0])))
+                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Org/" + str(row[0])))
                     name = row[1]
                     acronym = row[2]
                     type = row[3]
@@ -501,7 +802,7 @@ if __name__ == '__main__':
             with open('OrganizationalUnits.csv', 'w') as f:
                 print(",".join(("OrgUnitIdentifier", "Name", "Acronym", "Type")), file=f)
                 for row in rows:
-                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Org/" + str(row[0])))
+                    org_unit_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Org/" + str(row[0])))
                     name = row[1]
                     acronym = row[2]
                     type = row[3]
@@ -511,10 +812,10 @@ if __name__ == '__main__':
                     Account Data
             '''
             #   students
-            account_uri = "{0}ds/campusnexus/StudentCourses?$select=Student&$expand=Student($select=Id,FirstName,LastName,EmailAddress)&$top=10" \
+            account_uri = "{0}ds/campusnexus/StudentCourses?$select=Student&$expand=Student($select=Id,FirstName,LastName,EmailAddress){3}" \
                            "&$filter=Term/EndDate gt {1} and Term/StartDate le {2}" \
                            "".format(root_uri, now.strftime("%Y-%m-%d"),
-                                     (now + datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
+                                     (now + datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"), top)
 
             print(account_uri)
             r = s.get(account_uri)
@@ -565,8 +866,8 @@ if __name__ == '__main__':
             with open('Accounts.csv', 'w') as f:
                 print(",".join(("PersonIdentifier", "FirstName", "LastName", "Email")), file=f)
                 for row in rows:
-                    person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "Account/" + str(row[0])))
+                    person_identifier = str(uuid.uuid3(uuid.NAMESPACE_URL, "//lcu.edu/Account/" + str(row[0])))
                     first_name = row[1]
                     last_name = row[2]
                     email = row[3]
-                    print(",".join((person_identifier, section_identifier, first_name, last_name, email)), file=f)
+                    print(",".join((person_identifier, first_name, last_name, email)), file=f)
